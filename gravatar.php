bd5685b635d317b4eacb51184dcbcf454c64d534
Hayden Harnett <hayden@haydens-mbp-2.local>
2009-10-01 22:52:33 +1000

    Updated README and added Gravatar Helper

---
+++ gravatar.php
@@ -0,0 +1,139 @@
+<?php 
+/**
+ * Gravatar Helper
+ *
+ * @author Hayden Harnett
+ * @version 1.0
+ * @
+ * @url http://hayden.id.au/
+ */
+
+/**
+ * Import the Security Component
+ */
+App::import(array('Security'));
+
+class GravatarHelper extends AppHelper
+{
+	/**
+	 * Array of helpers needed
+	 *
+	 * @var array
+	 */
+	public $helpers = array('Html');
+	
+	/**
+	 * Base gravatar url
+	 *
+	 * @var string
+	 */
+	private $_url = 'http://www.gravatar.com/avatar/';
+	
+	/**
+	 * Size of the image
+	 *
+	 * @var string
+	 */
+	private $_size = 80;
+	
+	/**
+	 * Gravatar rating
+	 *
+	 * @var string
+	 */
+	private $_rating = 'pg';
+	
+	/**
+	 * Array of possible ratings
+	 *
+	 * @var string
+	 */
+	private $_possibleRatings = array('g', 'pg', 'r', 'x');
+	
+	/**
+	 * Default email hash
+	 *
+	 * @var string
+	 */
+	private $_default = 'ad516503a11cd5ca435acc9bb6523536';
+	
+	/**
+	 * Hash type used
+	 *
+	 * @var string
+	 */
+	private $_hashType = 'md5';
+	
+	/**
+	 * Validate the options array
+	 *
+	 * @param string $options 
+	 * @return void
+	 * @author Hayden Harnett
+	 */
+	private function _validateOptions($options)
+	{
+		if (isset($options['rating']))
+		{
+			if (in_array($_rating, $this->_possibleRatings))
+			{
+				$this->_rating = $options['rating'];
+			}
+		}
+			
+		if (isset($options['size']))
+		{
+			if (is_numeric($options['size']) && $options['size'] >=1 && $options['size'] <= 512)
+			{
+				$this->_size = $options['size'];
+			}
+		}
+		
+		if (isset($options['default']))
+		{
+			if (strpos('http://', $options['default']) == 0 || strpos('https://', $options['default'] == 0))
+			{
+				$this->_default = h($options['default']);
+			}
+			else
+			{
+				$this->_default = _hashEmail($options['default']);
+			}
+		}
+	}
+	
+	/**
+	 * Hash the email address
+	 *
+	 * @param string $email 
+	 * @return hash
+	 * @author Hayden Harnett
+	 */
+	private function _hashEmail($email)
+	{
+		return Security::hash(strtolower($email), $this->_hashType);
+	}
+	
+	/**
+	 * Public image function
+	 *
+	 * @param string $email 
+	 * @param array $options 
+	 * @return HTML image tag of the gravatar
+	 * @author Hayden Harnett
+	 */
+	public function image($email, $options)
+	{
+		$this->_validateOptions($options);
+		
+		$get = "?s=".$this->_size."&r=".$this->_rating;
+		
+		if (!empty($this->_default))
+		{
+			$get .= "&d=".$this->_default;
+		}
+		
+		return $this->Html->image($this->_url.$this->_hashEmail($email).$get, array('width' => $this->_size, 'height' => $this->_size));
+	}	
+}
+?>
